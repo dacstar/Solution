bb3b1bcdd49600f728e97ab80e2bdb07e2df3be2
dacstar <seheehan912@gmail.com>
2020-05-22 20:26:57 +0900

    20.05.22

--- BAEKJOON/Main1106.java
+++ BAEKJOON/Main1106.java
@@ -1,40 +1,41 @@
 package BAEKJOON;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main1106 {
+   static int C, N;
+   static int[] dp = new int[1001];
+   
+   
+   
+   public static void main(String[] args) {
+      Scanner s = new Scanner(System.in);
+      C=s.nextInt();
+      N=s.nextInt();
+      Arrays.fill(dp,Integer.MAX_VALUE);
+      for(int i=0;i<N;i++) {
+    	  int money=s.nextInt();
+    	  int people=s.nextInt();
+    	  dp[people]=money;
+      }
+      
+      for(int i=1;i<=1000;i++) {
+    	  int sum=Integer.MAX_VALUE;
+    	  for(int j=1;j<=i/2;j++) {
+    		  if(dp[j] == Integer.MAX_VALUE || dp[i-j] == Integer.MAX_VALUE)
+    			  continue;
+    		  sum=Math.min(sum, dp[j]+dp[i-j]);
+    	  }
+    	  dp[i]=Math.min(dp[i], sum);
+      }
+      
+      int result = Integer.MAX_VALUE;
+      for (int i = C; i < dp.length; i++) {
+         result = Math.min(dp[i], result);
+      }
+      System.out.println(result);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s = new Scanner(System.in);
-		int N=s.nextInt();
-		int C=s.nextInt();
-		int Arr[][] = new int[C][2];
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for(int i=0;i<C;i++) {
-			for(int j=0;j<2;j++) {
-				Arr[i][j]=s.nextInt();
-			}
-		}
-		int cnt=1;
-		int money=0;
-		int people=0;
-		boolean check=true;
-		while(true) {
-			
-			for(int i=0;i<C;i++) {
-				money+=(Arr[i][0]*cnt);
-				people+=(Arr[i][1]*cnt);
-				if(people==N) {
-					check=false;
-					break;
-				}
-			}
-			if(!check)
-				break;
-		}
-		System.out.println(money);
-	}
+   }
 
 }
